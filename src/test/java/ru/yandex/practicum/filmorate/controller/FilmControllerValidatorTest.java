381bd5b70d716c9398eb66faca68c564f1c8bca5
Kagermazov <kagermazoff@yandex.ru>
2024-05-28 11:34:11 +0200

    style: добавить отступ перед beforeEach() в FilmControllerValidatorTest

--- src/test/java/ru/yandex/practicum/filmorate/controller/FilmControllerValidatorTest.java
+++ src/test/java/ru/yandex/practicum/filmorate/controller/FilmControllerValidatorTest.java
@@ -14,6 +14,7 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 class FilmControllerValidatorTest {
 
     FilmController controller;
+
     @BeforeEach
     public void beforeEach() {
         this.controller = new FilmController();
